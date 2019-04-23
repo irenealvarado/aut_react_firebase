50e561dd73ff8a48f66bc663f371dcca85673bd2
nene29 <irenesalvarado29@gmail.com>
2019-04-23 10:14:43 -0400

    first commit

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+
+const config = {
+    apiKey: "AIzaSyB8uVFkZbLxcLCgG1_zaEXeyzK1zUk1Qr0",
+    authDomain: "authentwithreactandfirebase.firebaseapp.com",
+    databaseURL: "https://authentwithreactandfirebase.firebaseio.com",
+    projectId: "authentwithreactandfirebase",
+    storageBucket: "authentwithreactandfirebase.appspot.com",
+    messagingSenderId: "643604274800"
+};
