f0040f0875a8b48365443e059b8031b66b3f597e
nene29 <irenesalvarado29@gmail.com>
2019-04-24 11:53:45 -0400

    implemented authorization API for Firebase, not hooked to React yet

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,8 @@
 // Firebase configuration
 
+import app from 'firebase/app';
+import 'firebase/auth';
+
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -8,3 +11,27 @@ const config = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
+
+export default class Firebase {
+    constructor() {
+        app.initializeApp(config);
+
+        this.auth = app.auth();
+    }
+
+        // *** Auth API ***
+
+        doCreateUserWithEmailAndPassword = (email, password) =>
+            this.auth.createUserWithEmailAndPassword(email, password);
+
+        doSignInWithEmailAndPassword = (email, password) =>
+            this.auth.signInWithEmailAndPassword(email, password);
+
+        doSignOut = () => this.auth.signOut();
+
+        doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+        doPasswordUpdate = password =>
+            this.auth.currentUser.updatePassword(password);
+}
+
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//connect Firebase with React's context API to inject into components tree
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>,
+    document.getElementById('root'),
+);
 
 serviceWorker.unregister();
